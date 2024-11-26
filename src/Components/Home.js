53fdad0a572d729dbc614ec7770b8d54510169ea
Himal-Meghasuriya <143880458+Himal-Meghasuriya@users.noreply.github.com>
2024-11-26 15:12:21 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/Assets/Logo.svg
    	new file:   src/Assets/home-banner-background.png
    	new file:   src/Assets/home-banner-image.png
    	modified:   src/Components/Home.js
    	modified:   src/Components/Navbar.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import Navbar from './Navbar';
-
+import React from "react";
+import Navbar from "./Navbar";
+import bannerbackground from "../Assets/home-banner-background.png";
+import { FiArrowRight } from "react-icons/fi";
+import BannerImage from "../Assets/home-banner-image.png";
 const Home = () => {
   return (
-    <div className='Home-container'>
-        <Navbar />
+    <div className="Home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={bannerbackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Bot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />
+          </button>
+          <div className="home-image-container">
+            <img src={BannerImage} />
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default Home;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+import { useState } from "react";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 const Navbar = () => {
-  return (
-    <div className='first'>Navbar</div>
-  )
-}
+  const [OpenMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
 
-export default Navbar
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+
+      <div className="navbar-links-container">
+        {menuOptions.map((item) => {
+          return (
+            <>
+              <a>{item.text}</a>
+            </>
+          );
+        })}
+
+        <a href="">
+          <BsCart2 className="Navbar-cart-items" />
+        </a>
+
+        <button>Booking Now</button>
+      </div>
+
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu((prev) => !prev)} />
+      </div>
+
+      {OpenMenu && (
+        <>
+          <drawer
+            open={OpenMenu}
+            onClose={() => setOpenMenu(false)}
+            anchor="right"
+          >
+            <box>
+              <list>
+                {menuOptions.map((item) => (
+                  <ListItem key={item.text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.text} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </list>
+            </box>
+          </drawer>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
