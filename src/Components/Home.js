18fbe5b5ec66487797f02b3872a94d91960cc544
Himal-Meghasuriya <143880458+Himal-Meghasuriya@users.noreply.github.com>
2024-11-26 15:08:56 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div className='Home-container'>
+        <Navbar />
+    </div>
+)
+}
+
+export default Home;
